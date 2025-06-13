fbd17fd1bad4ea4e70b608840be84d24ad5cbe3a
Rafael <rafaelnunesdevasconcelos@gmail.com>
2025-06-13 15:40:41 -0300

    cafe

---
+++ app/src/main/java/com/rafaelnv/coffeeshop/Activity/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.rafaelnv.coffeeshop.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rafaelnv.coffeeshop.R
+import com.rafaelnv.coffeeshop.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener() {
+
+        }
+    }
+}
